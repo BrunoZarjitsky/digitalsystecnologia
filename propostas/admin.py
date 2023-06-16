51dd51186e9d7322a1de91a0e2e51a3f96b5a710
brunozarjitsky <brunozar@poli.ufrj.br>
2023-06-16 15:10:37 -0300

    make admin not editable after status settled

--- propostas/admin.py
+++ propostas/admin.py
@@ -1,7 +1,19 @@
+from typing import Optional
 from django.contrib import admin
+from django.http.request import HttpRequest
 from .models import Proposta
 
 
 @admin.register(Proposta)
 class PropostaAdmin(admin.ModelAdmin):
-    pass
+    # Torna o objeto não editavel quando aprovado ou negado
+    def get_readonly_fields(self, request: HttpRequest, obj=None):
+        if obj and obj.status != Proposta.StatusPossiveis.EM_ANALISE:
+            return [
+                'nome_completo',
+                'cpf',
+                'endereco',
+                'valor_emprestimo',
+                'status',
+            ]
+        return ['status']
