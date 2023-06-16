231700e2845b71656a962a2d98674bf51e464e27
brunozarjitsky <brunozar@poli.ufrj.br>
2023-06-16 17:28:21 -0300

    Created admin actions and new task

--- propostas/admin.py
+++ propostas/admin.py
@@ -1,7 +1,7 @@
-from typing import Optional
 from django.contrib import admin
 from django.http.request import HttpRequest
 from .models import Proposta
+from rabbit_utils.rabbit_utils import RabbitService
 
 
 @admin.register(Proposta)
@@ -17,3 +17,29 @@ class PropostaAdmin(admin.ModelAdmin):
                 'status',
             ]
         return ['status']
+
+    # Solicita a avaliação do objetos selecionados
+    @admin.action(description="Solicitar avaliação")
+    def solicitar_avaliacao(self, request, queryset):
+        propostas_atualizar = []
+        for proposta in queryset:
+            if proposta.status != Proposta.StatusPossiveis.EM_ANALISE:
+                continue
+            propostas_atualizar.append(proposta.id)
+        service = RabbitService()
+        message = service.create_message(
+            task="propostas.tasks.avalia_lista_propostas",
+            args=[propostas_atualizar]
+        )
+        service.publish_message(
+            queue='celery',
+            message=message
+        )
+        service.close_connection()
+
+    # Define o status dos objetos selecionados como em analise
+    @admin.action(description="Bota em análise")
+    def em_analise(self, request, queryset):
+        queryset.update(status=Proposta.StatusPossiveis.EM_ANALISE)
+
+    actions = ["solicitar_avaliacao", "em_analise"]
--- propostas/tasks.py
+++ propostas/tasks.py
@@ -15,3 +15,27 @@ def avalia_proposta(proposta_id):
     if total_aprovadas > total_negadas:
         proposta.status = Proposta.StatusPossiveis.NEGADA
     proposta.save(update_fields=['status'])
+
+
+@app.task
+def avalia_lista_propostas(lista_propostas_id):
+    propostas_atualizadas = []
+    todas_propostas = Proposta.objects.all()
+    total_aprovadas = todas_propostas.filter(
+        status=Proposta.StatusPossiveis.APROVADA).count()
+    total_negadas = todas_propostas.filter(
+        status=Proposta.StatusPossiveis.NEGADA).count()
+
+    for proposta_id in lista_propostas_id:
+        proposta = Proposta.objects.get(id=proposta_id)
+        proposta.status = Proposta.StatusPossiveis.APROVADA
+        total_aprovadas += 1
+        if total_aprovadas > total_negadas:
+            proposta.status = Proposta.StatusPossiveis.NEGADA
+            total_negadas += 1
+            total_aprovadas -= 1
+        propostas_atualizadas.append(proposta)
+    Proposta.objects.bulk_update(
+        propostas_atualizadas,
+        fields=['status']
+    )
