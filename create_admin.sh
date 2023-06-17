34c0bfd1cdcb110be13ab3c1e18fd98e268e2802
brunozarjitsky <brunozar@poli.ufrj.br>
2023-06-17 18:52:55 -0300

    created superuser container

---
+++ create_admin.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $DJANGO_SUPERUSER_USERNAME ];
+then
+    python manage.py createsuperuser \
+        --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL
+fi
